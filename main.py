87726de517ec45218bdde156708705505cd23ff5
Iain Stewart <iainstewart08@gmail.com>
2024-02-14 22:22:15 +0000

    Complete turtle crossing game

--- main.py
+++ main.py
@@ -8,7 +8,30 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # check for collision with car
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # detect successful crossing
+    if player.has_crossed_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
